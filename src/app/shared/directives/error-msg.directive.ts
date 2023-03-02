70452fefb41c7cbc94b3f9bb4ecf71aac49ed9b0
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-03-02 14:23:07 -0500

    form add

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,14 +1,25 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit {
 
-  constructor() { }
+  htmlElement: ElementRef<HTMLElement>;
+
+  constructor(
+    private element: ElementRef<HTMLElement>,
+  ) {
+    this.htmlElement = element;
+
+  }
 
   ngOnInit(): void {
-    
+    this.setColor();
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = 'red';
   }
 
 }
